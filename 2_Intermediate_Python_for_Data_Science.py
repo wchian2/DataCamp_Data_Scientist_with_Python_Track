fa85867d06bbb058be198ffc989c02f4a4691896
William W. Chiang <william.pharmd@gmail.com>
2018-09-08 11:16:57 -0500

    Onwards to Python dictionaries!

--- 2_Intermediate_Python_for_Data_Science.py
+++ 2_Intermediate_Python_for_Data_Science.py
@@ -344,3 +344,73 @@ print(cars) # when the data frame is printed out, it's missing the row index and
 # we can change that by revising the code on line 339 to:
 # cars = pd.read_csv('cars.csv', index_col=0)
 
+# Square Brackets (1)
+
+# Import cars data
+# import pandas as pd <- commented out; already imported
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+# print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+# print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+# print(cars[['country', 'drives_right']])
+
+# Square Brackets (2)
+
+# Import cars data
+# import pandas as pd <- commented out; already imported
+# cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out first 3 observations
+# print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+# print(cars[3:6])
+
+# loc and iloc (1)
+
+# Import cars data
+# import pandas as pd
+# cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out observation for Japan
+# print(cars.loc[['JAP']])
+# print(cars.iloc[[2]]) <- another equivalent
+
+# Print out observations for Australia and Egypt
+# print(cars.loc[['AUS', 'EG']])
+# print(cars.iloc[[1,6]]) <- another equivalent
+
+# loc and iloc (2)
+
+# Import cars data
+# import pandas as pd
+# cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right value of Morocco
+# print(cars.loc['MOR', 'drives_right'])
+
+# Print sub-DataFrame
+# print(cars.loc[['RU', 'MOR'],['country', 'drives_right']])
+
+# loc and iloc (3)
+
+# Import cars data
+# import pandas as pd
+# cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+# print(cars.loc[:, 'drives_right'])
+
+# Print out drives_right column as DataFrame
+# print(cars.loc[:, ['drives_right']])
+
+# Print out cars_per_cap and drives_right as DataFrame
+# print(cars.loc[:, ['cars_per_cap', 'drives_right']])
+
+## 3 - Logic, Control Flow and Filtering
+
