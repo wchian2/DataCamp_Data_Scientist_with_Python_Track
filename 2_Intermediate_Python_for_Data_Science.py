a420af6f768e9228f1a318850abba19fc2ea639a
William W. Chiang <william.pharmd@gmail.com>
2018-09-08 22:55:18 -0500

    Onwards to Python dictionaries!

--- 2_Intermediate_Python_for_Data_Science.py
+++ 2_Intermediate_Python_for_Data_Science.py
@@ -693,3 +693,133 @@ for x in np.nditer(np_baseball):
 
 # 5 - Case Study: Hacker Statistics
 
+# Random float
+
+# Import numpy as np
+# import numpy as np
+
+# Set the seed
+np.random.seed(123)
+
+# Generate and print random float
+print(np.random.rand())
+
+# Roll the dice
+
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice <- prints a random number between 1-6
+print(np.random.randint(1,7))
+
+# Use randint() again
+print(np.random.randint(1,7))
+
+# Determine your next move
+
+# Import numpy and set seed
+# import numpy as np
+np.random.seed(123)
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5:
+    step = step + 1
+else:
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
+
+# The next step
+
+# Import numpy and set seed
+#import numpy as np
+np.random.seed(123)
+
+# Initialize random_walk
+random_walk = [0]
+
+# Complete the ___
+for x in range(100) :
+    # Set step: last element in random_walk
+    step = random_walk[-1]
+
+    # Roll the dice
+    dice = np.random.randint(1,7)
+
+    # Determine next step
+    if dice <= 2:
+        # step = step - 1 <- this line of code potentially generates a negative step
+        step = max(0, step - 1)  # correction: the max function returns the max number of the two arguments
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    # append next_step to random_walk
+    random_walk.append(step)
+
+# Print random_walk
+print(random_walk)
+
+# Visualize the walk
+plt.plot(random_walk) # if one parameter is provided in the plot function, the x will be the index of list, and the y are the list values
+plt.show()
+
+# Simulate multiple walks: a for-loop in a for-loop
+
+# Initialization
+# import numpy as np
+np.random.seed(123)
+
+# Initialize all_walks
+all_walks = []
+
+# Simulate random walk 10 times
+for i in range(10) :
+
+    # Code from before
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if np.random.rand() <= 0.001:
+            step = 0
+
+        random_walk.append(step)
+
+    # Append random_walk to all_walks
+    all_walks.append(random_walk)
+
+# Print all_walks
+print(all_walks)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1]
+
+# Plot histogram of ends, display plot
+plt.hist(ends)
+plt.show()
+
