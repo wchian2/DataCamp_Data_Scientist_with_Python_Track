62c998870cecb5b88a4a51885b517c8cf2c82f83
William W. Chiang <william.pharmd@gmail.com>
2018-09-09 16:59:29 -0500

    Module 3

---
+++ 3_Python_Data_Science_Toolbox_1.py
@@ -0,0 +1,47 @@
+# 1 - Writing your own functions
+
+# Write a simple function
+
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout()
+
+# Single-parameter functions
+
+# Define shout with the parameter, word
+def shout(word):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout("congratulations")
+
+# Functions that return single values
+
+# Define shout with the parameter, word
+def shout(word):
+    """Return a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Replace print with return
+    return shout_word
+
+# Pass 'congratulations' to shout: yell
+yell = shout('congratulations')
+
+# Print yell
+print(yell)
+
