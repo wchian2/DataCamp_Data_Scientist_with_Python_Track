0b3814d5dcc78d4c52e293e2ea0c5c7c0d348038
William W. Chiang <william.pharmd@gmail.com>
2018-09-09 22:10:04 -0500

    Module 3

--- 3_Python_Data_Science_Toolbox_1.py
+++ 3_Python_Data_Science_Toolbox_1.py
@@ -69,6 +69,7 @@ yell = shout('congratulations', 'you')
 print(yell)
 
 # A brief introduction to tuples
+nums = (3, 4, 6)
 
 # Unpack nums into num1, num2, and num3
 num1, num2, num3 = nums
@@ -106,15 +107,16 @@ print(yell2)
 import pandas as pd
 
 # Import Twitter data as DataFrame: df
-df = pd.read_csv('tweets.csv')
+#df = pd.read_csv('tweets.csv')
 
 # Initialize an empty dictionary: langs_count
-langs_count = {}
+#langs_count = {}
 
 # Extract column from DataFrame: col
-col = df['lang']
+#col = df['lang']
 
 # Iterate over lang column in DataFrame
+"""
 for entry in col:
 
     # If the language is in langs_count, add 1
@@ -126,6 +128,7 @@ for entry in col:
 
 # Print the populated dictionary
 print(langs_count)
+"""
 
 # Briging it all together <- adapting the previous codes into a function
 
@@ -155,7 +158,337 @@ def count_entries(df, col_name):
 
 
 # Call count_entries(): result
-result = count_entries(tweets_df, 'lang')
+#result = count_entries(tweets_df, 'lang')
+
+# Print the result
+#print(result)
+
+# 2 - Default arguments, variable-length arguments and scope
+
+# The keyword global
+
+# Create a string: team
+team = "teen titans"
+
+
+# Define change_team()
+def change_team():
+    """Change the value of the global variable team."""
+
+    # Use team in global scope
+    global team
+
+    # Change the value of team in global: team
+    team = "justice league"
+
+
+# Print team
+print(team)
+
+# Call change_team()
+change_team()
+
+# Print team
+print(team)
+
+# Python's built-in scope
+# Create a string: team
+team = "teen titans"
+
+
+# Define change_team()
+def change_team():
+    """Change the value of the global variable team."""
+
+    # Use team in global scope
+    global team
+
+    # Change the value of team in global: team
+    team = "justice league"
+
+
+# Print team
+print(team)
+
+# Call change_team()
+change_team()
+
+# Print team
+print(team)
+#import builtins
+#dir(builtins)
+
+# Nested Functions I
+
+# Define three_shouts
+def three_shouts(word1, word2, word3):
+    """Returns a tuple of strings
+    concatenated with '!!!'."""
+
+    # Define inner
+    def inner(word):
+        """Returns a string concatenated with '!!!'."""
+        return word + '!!!'
+
+    # Return a tuple of strings
+    return (inner(word1), inner(word2), inner(word3))
+
+# Call three_shouts() and print
+print(three_shouts('a', 'b', 'c')) # returns a tuple of 3 elements ('a!!!', 'b!!!', 'c!!!')
+
+# Nested Functions II
+
+# Define echo
+def echo(n):
+    """Return the inner_echo function."""
+
+    # Define inner_echo
+    def inner_echo(word1):
+        """Concatenate n copies of word1."""
+        echo_word = word1 * n
+        return echo_word
+
+    # Return inner_echo
+    return inner_echo
+
+# Call echo: twice
+twice = echo(2)
+
+# Call echo: thrice
+thrice = echo(3)
+
+# Call twice() and thrice() then print
+print(twice('hello'), thrice('hello'))
+
+# The keyword nonlocal and nested functions
+
+# Define echo_shout()
+#def echo_shout(word):
+    #"""Change the value of a nonlocal variable"""
+
+    # Concatenate word with itself: echo_word
+    #echo_word = word * 2
+
+    # Print echo_word
+    #print(echo_word)
+
+    # Define inner function shout()
+    #def shout():
+        #"""Alter a variable #in the enclosing scope"""
+        # Use echo_word in nonlocal scope
+        #nonlocal echo_word
+
+        #echo_word =  echo_word + '!!!'
+
+    # Call function shout()
+   # shout()
+
+    # Print echo_word
+    #print(echo_word)
+
+
+# Call function echo_shout() with argument 'hello'
+#echo_shout('hello')
+
+# Functions with one default argument
+
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+     exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo() with "Hey": no_echo
+no_echo = shout_echo("Hey")
+
+# Call shout_echo() with "Hey" and echo=5: with_echo
+with_echo = shout_echo("Hey", 5)
+
+# Print no_echo and with_echo
+print(no_echo)
+print(with_echo)
+
+# Functions with multiple default arguments
+
+# Define shout_echo
+def shout_echo(word1, echo=1, intense=False):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Capitalize echo_word if intense is True
+    if intense is True:
+        # Capitalize and concatenate '!!!': echo_word_new
+        echo_word_new = echo_word.upper() + '!!!'
+    else:
+        # Concatenate '!!!' to echo_word: echo_word_new
+        echo_word_new = echo_word + '!!!'
+
+    # Return echo_word_new
+    return echo_word_new
+
+# Call shout_echo() with "Hey", echo=5 and intense=True: with_big_echo
+with_big_echo = shout_echo("Hey", 5, True)
+
+# Call shout_echo() with "Hey" and intense=True: big_no_echo
+big_no_echo = shout_echo("Hey", 1, True)
+
+# Print values
+print(with_big_echo)
+print(big_no_echo)
+
+# Functions with variable-length arguments (*args)
+
+# Define gibberish
+def gibberish(*args):
+    """Concatenate strings in *args together."""
+
+    # Initialize an empty string: hodgepodge
+    hodgepodge = ""
+
+    # Concatenate the strings in args
+    for word in args:
+        hodgepodge += word
+
+    # Return hodgepodge
+    return hodgepodge
+
+# Call gibberish() with one string: one_word
+one_word = gibberish("luke")
+
+# Call gibberish() with five strings: many_words
+many_words = gibberish("luke", "leia", "han", "obi", "darth")
+
+# Print one_word and many_words
+print(one_word)
+print(many_words)
+
+# Functions with variable-length keyword arguments (**kwargs)
+
+# Define report_status
+def report_status(**kwargs):
+    """Print out the status of a movie character."""
+
+    print("\nBEGIN: REPORT\n")
+
+    # Iterate over the key-value pairs of kwargs
+    for key, value in kwargs.items():
+        # Print out the keys and values, separated by a colon ':'
+        print(key + ": " + value)
+
+    print("\nEND REPORT")
+
+# First call to report_status()
+report_status(name="luke", affiliation="jedi", status="missing")
+
+# Second call to report_status()
+report_status(name="anakin", affiliation="sith lord", status="deceased")
+
+# Bringing it all together (1)
+
+"""
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+
+    # Extract column from DataFrame: col
+    col = df[col_name]
+
+    # Iterate over the column in DataFrame
+    for entry in col:
+
+        # If entry is in cols_count, add 1
+        if entry in cols_count.keys():
+            cols_count[entry] += 1
+
+        # Else add the entry to cols_count, set the value to 1
+        else:
+            cols_count[entry] = 1
+
+    # Return the cols_count dictionary
+    return cols_count
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df)
+
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, 'source')
+
+# Print result1 and result2
+print(result1)
+print(result2)
+"""
+
+# 3 - Lambda functions and error-handling
+
+# Writing a lambda function you already know
+
+"""
+def echo_word(word1, echo):
+
+    words = word1 * echo
+    return words
+"""
+
+# Define echo_word as a lambda function: echo_word
+echo_word = (lambda word1, echo: word1 * echo)
+
+# Call echo_word: result
+result = echo_word('hey', 5)
+
+# Print result
+print(result)
+
+# Map() and lambda functions
+
+# Create a list of strings: spells
+spells = ["protego", "accio", "expecto patronum", "legilimens"]
+
+# Use map() to apply a lambda function over spells: shout_spells
+shout_spells = map(lambda item: item + '!!!', spells)
+
+# Convert shout_spells to a list: shout_spells_list
+shout_spells_list = list(shout_spells)
+
+# Convert shout_spells into a list and print it
+print(shout_spells_list)
+
+# Filter() and lambda functions
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda member: len(member) > 6, fellowship)
+
+# Convert result to a list: result_list
+result_list = list(result)
+
+# Convert result into a list and print it
+print(result_list)
+
+# Reduce() and lambda functions
+
+# Import reduce from functools
+from functools import reduce
+
+# Create a list of strings: stark
+stark = ['robb', 'sansa', 'arya', 'brandon', 'rickon']
+
+# Use reduce() to apply a lambda function over stark: result
+result = reduce(lambda item1, item2: item1 + item2, stark)
 
 # Print the result
 print(result)
